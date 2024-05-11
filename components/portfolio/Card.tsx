0b3a1344786a3f6022d7ce252f0f505378682296
Sangye Tashi <sangyetashi2@gmail.com>
2024-05-11 09:31:26 +0530

    add animation to project section

--- components/portfolio/Card.tsx
+++ components/portfolio/Card.tsx
@@ -1,19 +1,31 @@
+"use client";
 import Image, { StaticImageData } from "next/image";
 import React from "react";
-import screenShot from "../../public/images/unsplashScreenshot.png";
-
+import { Variant, Variants, motion } from "framer-motion";
 type CardProps = {
   title: string;
   url: string;
   description: string;
   image: StaticImageData;
+  variant?: Variants;
 };
 
-const Card: React.FC<CardProps> = ({ title, url, description, image }) => {
+const cardVariant: Variants = {
+  visible: { y: 0, opacity: 1 },
+  hidden: { y: 50, opacity: 0 },
+};
+
+const Card: React.FC<CardProps> = ({
+  variant,
+  title,
+  url,
+  description,
+  image,
+}) => {
   return (
-    <a href={url}>
+    <motion.a href={url} viewport={{ once: true }} variants={variant}>
       <div className="group relative  cursor-pointer overflow-hidden rounded-xl object-cover  outline outline-1 outline-gray-100 transition-all duration-1000">
-        <div className="absolute inset-0 flex flex-col justify-end space-y-3 bg-gradient-to-b from-gray-950/30 to-gray-950 p-4 hover:backdrop-blur-sm ">
+        <div className="absolute inset-0 flex flex-col justify-end space-y-3 bg-gradient-to-b from-gray-950/30 to-black p-4 hover:backdrop-blur-sm ">
           <h4 className="text-2xl font-semibold">{title}</h4>
           <p>{description}</p>
         </div>
@@ -24,7 +36,7 @@ const Card: React.FC<CardProps> = ({ title, url, description, image }) => {
           alt="website screenshot image"
         />
       </div>
-    </a>
+    </motion.a>
   );
 };
 export default Card;
--- components/portfolio/Projects.tsx
+++ components/portfolio/Projects.tsx
@@ -1,43 +1,91 @@
+"use client";
 import React from "react";
+import { motion, Variants } from "framer-motion";
 import Card from "./Card";
-
 import superChatImage from "../../public/images/superChatScreenshot.png";
 import unsplashImage from "../../public/images/unsplashScreenshot.png";
 import todoImage from "../../public/images/todoAppScreenShot.png";
 import twitterUiImage from "../../public/images/twitterCloneScreenshot.png";
 type ProjectsProps = {};
 
+const cardVariant: Variants = {
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: { duration: 0.8, type: "spring", stiffness: 300, damping: 24 },
+  },
+  hidden: { y: 50, opacity: 0 },
+};
+const projectTitleVariant: Variants = {
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: { duration: 0.8, type: "spring", stiffness: 300, damping: 24 },
+  },
+  hidden: { y: 50, opacity: 0 },
+};
+
 const Projects: React.FC<ProjectsProps> = () => {
   return (
-    <div className="flex flex-col items-center space-y-20">
-      <h3 className="text-5xl font-bold">Projects</h3>
-      <div className="grid gap-12 md:grid-cols-2 lg:grid-cols-3">
+    <motion.div className="flex flex-col items-center space-y-20">
+      <motion.h3
+        variants={projectTitleVariant}
+        initial={"hidden"}
+        whileInView={"visible"}
+        className="text-5xl font-bold"
+      >
+        Projects
+      </motion.h3>
+      <motion.div
+        variants={{
+          ...cardVariant,
+          visible: {
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 0.8,
+              type: "spring",
+              stiffness: 300,
+              damping: 24,
+              staggerChildren: 0.2,
+              delayChildren: 0.1,
+            },
+          },
+        }}
+        initial={"hidden"}
+        whileInView={"visible"}
+        className="grid gap-12 md:grid-cols-2 lg:grid-cols-3"
+      >
         <Card
           title="Unsplash Image Search "
           url={"https://my-project-builds.web.app/"}
           description="Image Search and download app built with unsplash API, TailwindCSS, React"
           image={unsplashImage}
+          variant={cardVariant}
         />
         <Card
+          variant={cardVariant}
           title={"Super Chat"}
           url="https://chat-app-4e4b7.web.app/"
           image={superChatImage}
           description="Public Superchat built with firebase realtime database and authentication with ReactJS, TailwindCSS, Recoil"
         />
         <Card
+          variant={cardVariant}
           title="Simple Todo App "
           url={"https://sangye-todos-app.netlify.app/"}
           description="Local Storage todo app built with React, Tailwind and FramerMotion"
           image={todoImage}
         />
         <Card
+          variant={cardVariant}
           title="Twitter UI"
           url={"https://sangye-twitter-ui-clone.netlify.app/"}
           description="Twitter homepage's visual clone with TailwindCSS with random photo and username api using axios "
           image={twitterUiImage}
         />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 export default Projects;
